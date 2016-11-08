29782ab99ee16d77335c4c30772ace565c884ee7
Nealon Suthersan <nealonsuthersan@Nealons-MacBook-Pro.local>
2016-11-07 20:11:16 -0500

    third commit

--- twitterhw3a.py
+++ twitterhw3a.py
@@ -3,6 +3,17 @@
 # hashtags #UMSI-206 #Proj3 in the tweet.
 
 # You will demo this live for grading.
+import tweepy
 
-print("""No output necessary although you 
-	can print out a success/failure message if you want to.""")
+access_token = "2993514232-ZFOQSjwBwT9Dj127B9rtHF2hkLgTRMHEY8f50Ch"
+access_token_secret = "qeZH5nk5Rry4DlCSIoukvAl9BIGsFMmR24LxrTw5zD5fn"
+consumer_key = "HSNLNuIvUjZ5NdKTZQ6Dpbeul"
+consumer_secret = "bJ0w6JkanBdmLQN7B8Cyl7dw6RLvFFFqlbA65xKOPsBdrCiQvO"
+
+auth1 = tweepy.OAuthHandler(consumer_key,consumer_secret)
+auth1.set_access_token(access_token,access_token_secret)
+
+api_auth = tweepy.API(auth1)
+
+status = "#UMSI206 #Proj3"
+api_auth.update_with_media('Salad.jpg', status=status)
